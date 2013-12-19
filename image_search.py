56ba20e42deb63672e62954939424ecfc68f3469
radicalcakes <radicalcakes@Johns-MacBook-Air.local>
2013-12-18 19:25:37 -0500

    added func to return image urls

--- image_search.py
+++ image_search.py
@@ -10,10 +10,12 @@ def search(image_url):
         data = file(cache_file).read().decode('utf8')
     else:
         headers = {}
-        headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
-        response = requests.get("https://www.google.com/searchbyimage?image_url=" + image_url, headers=headers)
+        headers[
+            'User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+        response = requests.get(
+            "https://www.google.com/searchbyimage?image_url=" + image_url, headers=headers)
         data = response.text
-        file = open(cache_file,"wb").write(data.encode('utf8'))
+        file = open(cache_file, "wb").write(data.encode('utf8'))
 
     result_partition = data.split("Pages that include")
     if len(result_partition) != 2:
@@ -26,18 +28,22 @@ def search(image_url):
     return out
 
 
+# returns a list of image elements from a url
 def get_images(url):
     http = urllib3.PoolManager()
     headers = {}
-    headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+    headers[
+        'User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
     response = http.request("GET", url, headers=headers)
     soup = bs4.BeautifulSoup(response.data)
-    return soup.find_all("img")
+    img_urls = []
+    for img in soup.find_all("img"):
+        if img['src']:
+            img_urls.append(img['src'])
+    return img_urls
 
 
 if __name__ == "__main__":
     image_url = "http://i0.kym-cdn.com/photos/images/original/000/000/130/disaster-girl.jpg"
     for url in search(image_url):
         print(get_images(url))
-
-    
