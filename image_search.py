4c87c3b4adb7c0439b93d2e1072020af7d8234d1
Snail <snail@ubuntu.(none)>
2013-12-18 23:28:54 -0800

    V1 working

--- image_search.py
+++ image_search.py
@@ -1,31 +1,54 @@
 import os
-import requests
+import urllib2
+import urlparse
 
+opener = urllib2.build_opener()
+opener.addheaders = [('User-Agent',  "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17")]
 
-def search(image_url):
-    cache_file = "cache/%s.html" % abs(hash(image_url))
+CACHE_DIR = "cache"
+
+if not os.path.exists(CACHE_DIR):
+    os.makedirs(CACHE_DIR)
+
+def get_url_data(url):
+    cache_file = os.path.join(CACHE_DIR, "C%s.html" % hash(url))
     if os.path.exists(cache_file):
-        data = file(cache_file).read().decode('utf8')
+	print "Found in cache"
+        data = file(cache_file).read()
     else:
         headers = {}
-        headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
-        response = requests.get("https://www.google.com/searchbyimage?image_url=" + image_url, headers=headers)
-        data = response.text
-        file(cache_file,"w").write(data.encode('utf8'))
+        data = opener.open(url).read()
+	f = file(cache_file, "w")
+	f.write(data)
+	f.close()
+    return data
 
+def search(image_url):
+    data = get_url_data("https://www.google.com/searchbyimage?image_url=" + image_url)
     result_partition = data.split("Pages that include")
     if len(result_partition) != 2:
+	print "Pages that include was not found"
         return []
     data = result_partition[1]
     link_blobs = data.split('"r"><a href="')
     out = []
+    print "Link blob partitions:", len(link_blobs)
     for blob in link_blobs[1:]:
         out.append(blob.split('"')[0])
+    print "Returning"
+    print out
     return out
 
-
-
-
+def scrape(url):
+    data = get_url_data(url)
+    img_tags = data.split("img src=")[1:]
+    img_srcs= []
+    for imgtag in img_tags:
+        delim = imgtag[0]
+        img_path = imgtag.split(delim)[1]
+        img_src = urlparse.urljoin(url, img_path)
+        img_srcs.append(img_src)
+    return img_srcs
 if __name__ == "__main__":
     image_url = "http://i0.kym-cdn.com/photos/images/original/000/000/130/disaster-girl.jpg"
     for url in search(image_url):
--- main.js
+++ main.js
@@ -1,8 +1,64 @@
+
+function search_input_image() {
+  var image_url = $("#img_input").val()
+  console.log(image_url)
+  $("#out").empty();
+  $.post("search", image_url, on_search_results);
+}
+function on_search_results(data) { 
+  console.log(arguments)
+  console.log("search results: ", unescape(data))
+  var urls = JSON.parse(unescape(data))
+  for (var key in urls) {
+    console.log(urls,key,urls[key])
+    $.post("scrape", urls[key], on_scrape_results);
+  }
+}
+function on_scrape_results(data) {
+  var urls = JSON.parse(unescape(data))
+  for (var key in urls) {
+    append_image(urls[key])
+  }
+}
+
+function append_image(path) {
+
+$('<img />').attr({
+  src:path
+  //width:'100',
+  //height:'100'
+}).click(function(e){
+  $("#img_input").val(path);
+  search_input_image();
+})
+.appendTo($('<a />').attr({
+  href:'#' + path
+}).appendTo($('#out')));
+
+/*
+    $('<img src="'+ path +'">').load(function() {
+	var me =  $(this);
+	me = me.wrap("<a href='"+path+"'></a>");
+	me.appendTo($("body"));
+      //$(this).appendTo($("body"));
+	//	.width(width).height(height)
+    });
+*/
+    //$("a").attr("href", "#" + image_url).append(
+}
+
 $(function() {
+  $("#img_input").keypress(function (e) {
+    if (e.which == 13) {
+      search_input_image()
+    }
+  });
   // Get the image url from the location's hash
   var image_url = location.hash.substring(1);
-  console.log("Image url:", image_url);
-
+  if (image_url !== "") {
+    $("#img_input").val(image_url)
+    search_input_image()
+  }
   // Search by that url
   // For each of the returned urls:
   //   Get that page
