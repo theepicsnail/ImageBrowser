6551ef6a11bcdb376234c262d7a9691429177c6f
Steven <theepicsnail@gmail.com>
2013-12-20 14:37:55 -0500

    Added exception handling, defaults to empty list

--- main.py
+++ main.py
@@ -5,21 +5,24 @@ from image_search import search, scrape
 import json
 
 class Server(SimpleHTTPServer.SimpleHTTPRequestHandler):
-	def do_POST(self):
-		operation = self.path[1:]
-		length = int(self.headers.getheader('content-length'))        
-		arg = self.rfile.read(length)
-		if operation == "search":
-			result = search(arg)
-			#result = urllib.quote(result, "") #urllib.urlencode({"results":result})
-			#result = "\n".join(map(urllib.urlencode,enumerate(search(arg))))
-		elif operation == "scrape":
-			result = scrape(arg)
-		out =urllib.quote(json.dumps(result), "")
-		self.wfile.write(out)
+    def do_POST(self):
+        out = "[]"
+        try:
+            operation = self.path[1:]
+            length = int(self.headers.getheader('content-length'))        
+            arg = self.rfile.read(length)
+            print operation, arg
+            if operation == "search":
+                result = search(arg)
+            elif operation == "scrape":
+                result = scrape(arg)
+            out = urllib.quote(json.dumps(result), "")
+        except: pass
+        self.wfile.write(out)
 
 
-httpd = SocketServer.ThreadingTCPServer(("",0), Server)
+PORT = 12347
+httpd = SocketServer.ThreadingTCPServer(("",PORT), Server)
 
 PORT = httpd.socket.getsockname()[1]
 image = "http://www.freefever.com/stock/rainbow-abstract-background-free.jpg"
